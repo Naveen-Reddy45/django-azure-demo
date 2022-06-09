bc7d870fd372bc65ee5f62de60bde8b8670ad9ac
unknown <naveenreddyg3954@gmail.com>
2022-06-09 11:26:14 +0000

    second commit

--- dell/dell/settings.py
+++ dell/dell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1qep&2l88$#e_cgd!8+#0s%81r^y@%*45wa=xs&6vls17cbk(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
