3b6cb5029f1b49a0da68ca71a223b4c2a1ace1c6
unknown <naveenreddyg3954@gmail.com>
2022-06-09 10:59:28 +0000

    first commit

---
+++ dell/ind/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html', {'name': 'Naveen'})
+
+def add(request):
+    n1 = int(request.GET['num1'])
+    n2 = int(request.GET('num2'))
+    r = n1+n2
+    return render(request, 'res.html', {'result': 'r'})
